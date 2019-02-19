5d62ab609a333e0b2f39982ad19ca6db23b69f7c
jn <tupiszakaczki@gmail.com>
2019-02-19 13:56:30 +0100

    update repo name

--- _deploy.sh
+++ _deploy.sh
@@ -8,7 +8,7 @@ update_website() {
   git config user.name "jn"
   git config user.email "tupiszakaczki@gmail.com"
   git config --global push.default simple
-  git remote add upstream "https://$GH_TOKEN@github.com/Nowosad/my_repo.git"
+  git remote add upstream "https://$GH_TOKEN@github.com/Nowosad/elp.git"
   git fetch upstream 2>err.txt
   git checkout -f gh-pages
   
