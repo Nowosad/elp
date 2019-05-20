e12a6641c6c6bf676cb886747164e820194a367a
Nowosad <tupiszakaczki@gmail.com>
2019-05-20 13:48:57 +0200

    next ex

--- ex_tmp.R
+++ ex_tmp.R
@@ -69,15 +69,45 @@ dir_name = "pliki"
 
 # 9.8
 # W pliku list.txt znajduje się zaszyfrowana wiadomość. Aby ją odczytać należy stworzyć odwrotność jej zawartości, tj. pierwsza linia tekstu w pliku wejściowym ma być ostatnią linią tekstu w przetworzonym obiekcie, a pierwszy znak w danej linii ma stać się ostatnim, itd. Napisz funkcję, która odszyfruje tą wiadomość.
-???
+tekst = readLines("pliki/list.txt")
+stringi::stri_reverse(tekst)[length(tekst):1]
 
 # 4.3
 # Napisz funkcję, która przyjmuje trzy zmienne logiczne x, y i z. Jeżeli tylko jedna lub trzy ze zmiennych ma wartość TRUE wyświetl tekst "Nieparzysta liczba.", natomiast jeżeli dwie zmienne mają wartość TRUE wyświetl tekst "Parzysta liczba."
-???
+trzy_zmienne = function(x, y, z){
+  how_many_true = sum(c(x, y, z))
+  if (how_many_true %in% c(1, 3)){
+    print("Nieparzysta liczba.")
+  } else if (how_many_true == 2){
+    print("Parzysta liczba.")    
+  }
+}
+
+trzy_zmienne(TRUE, TRUE, FALSE)
 
 # 6.7
 # Stwórz funkcję, która przyjmując przykładowe dane z poprzedniego zadania zamieni współrzędne na format w postaci stopni dziesiętnych (np. 52°24′N w formacie DMS to 52.4 w stopniach dziesiętnych). 
-???
+wsp = c("52°24′N 16°55′E", "53°08′07″N 23°08′44″E", "39°6′N 84°31′W")
+x = wsp
+
+format_coords = function(x){
+  # direction = stringr::str_extract_all(x, pattern = "[NSEW]")
+  coords = stringr::str_extract_all(x, pattern = "([0-9]|[ ])")
+  coords = lapply(coords, as.numeric)
+  coords = lapply(coords, paste0, collapse = "")
+  coords = stringr::str_split(coords, pattern = "NA")
+  lat = unlist(lapply(coords, "[", 1))
+  lon = unlist(lapply(coords, "[", 2))
+  
+  df = data.frame(lat = lat, lon = lon)
+  df$lat_st = as.numeric(stringr::str_sub(df$lat, 1, 2))
+  df$lat_mi = as.numeric(stringr::str_sub(df$lat, 3, 4))
+  df$lat_se = as.numeric(stringr::str_sub(df$lat, 5, 6))
+  d + (min/60) + (sec/3600)
+  df$lat_res = df$lat_st + (df$lat_mi / 60) + ifelse(is.na(df$lat_se), 0, (df$lat_se / 3600))
+  
+  df
+}
 
 # 7.8
 # Stwórz nową ramkę danych, ra1, która składa się z dwóch kolumn i trzech wierszy. Pierwsza kolumna data zawiera datę z dziś, wczoraj i przedwczoraj, a kolumna miasto zawiera nazwę miasta w którym się właśnie znajdujesz.
